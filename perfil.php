34b5ea6928c33239805af9f4dd462f5093b2c594
kennyF01 <kennyofi@hotmail.com>
2025-04-23 20:51:27 -0500

    menu hamburguesa de perfil

--- perfil.php
+++ perfil.php
@@ -41,15 +41,27 @@ $conn->close();
     <title>Perfil - Misae Solemnes</title>
     <link rel="stylesheet" href="css/perfil.css">
     <link rel="icon" href="assets/icon/cruzar (1).png">
+    <script src="js/menu.js" defer></script>
 </head>
 <body>
     <header>
         <h1>MISAE SOLEMNES ✝️</h1>
-        <nav>
+        <div class="menu-container">
+            <a id="toggle" href="#"><span></span></a>
+            <div id="menu">
+              <ul>
+                <li><a href="pos/inicio.html">INICIO</a></li>
+                <li><a href="pos/acercaDe.html">ACERCA DE</a></li>
+                <li><a href="pos/transmisiones.php">TRANSMISIONES</a></li>
+                <li><a href="pos/logout.html">CERRAR SESIÓN</a></li>
+              </ul>
+            </div>
+        </div>
+        <nav class="menu">
             <a href="pos/inicio.html">INICIO</a>
-            <a href="">ACERCA DE</a>
+            <a href="pos/acercaDe.html">ACERCA DE</a>
             <a href="pos/transmisiones.php">TRANSMISIONES</a>
-            <a href="/pos/logout.html">CERRAR SESIÓN</a>
+            <a href="pos/logout.html">CERRAR SESIÓN</a>
         </nav>
     </header>
 
